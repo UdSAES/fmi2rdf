6da96a6fa070649cea5ba0f2d8ac5d1765aa1738
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-09-07 17:03:09 +0200

    Parse units, types, variables and relate them to each other (WIP)
    Incomplete, but a hopefully a good start.

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@
 
 """Translation of information about a FMU to RDF"""
 
+import json
 import os
 
 import fmpy
@@ -111,7 +112,109 @@ def assemble_graph(ctx, fmu_path, blackbox=False):
     for p, o, d in mappings:
         graph.add((fmu_uriref, p, rdflib.Literal(o, datatype=d)))
 
+    # Identify and parse units defined within the FMU
+    units_map = {}
+    for unit in md.unitDefinitions:
+        logger.debug(f"Parsing unit `{unit.name}`...")
+
+        unit_iri = f"{fmu_iri}/units#{unit.name.replace('/', '_')}"
+        unit_uriref = rdflib.URIRef(unit_iri)
+        logger.trace(unit_iri)
+
+        units_map[unit.name] = unit_uriref
+        graph.add((unit_uriref, RDF.type, FMI.Unit))
+
+    logger.debug(json.dumps(units_map, indent=2))
+
+    # Identify and parse types defined within the FMU
+    types_map = {}
+    for type in md.typeDefinitions:
+        logger.debug(f"Parsing type `{type.name}`...")
+
+        type_iri = f"{fmu_iri}/types#{type.name}"
+        type_uriref = rdflib.URIRef(type_iri)
+        logger.trace(type_iri)
+
+        types_map[type.name] = type_uriref
+        graph.add((type_uriref, RDF.type, FMI.SimpleType))
+
+        graph = add_variable_constraints(graph, units_map, type_uriref, type)
+
+    logger.debug(json.dumps(types_map, indent=2))
+
+    # Identify and parse variables defined within the FMU
+    for var in md.modelVariables:
+        logger.debug(f"Parsing variable `{var.name}`...")
+
+        var_iri = f"{fmu_iri}/variables#{var.name}"
+        var_uriref = rdflib.URIRef(var_iri)
+        logger.trace(var_iri)
+
+        # Distinguish between parameters, inputs, outputs, and internal variables
+        if var.causality in ["parameter", "calculatedParameter", "input", "output"]:
+            if var.causality == "parameter" or var.causality == "calculatedParameter":
+                if blackbox == True:
+                    # Only parse top-level parameters
+                    # XXX Assumes hierarchical component names!
+                    if not ("." in var.name):
+                        graph.add((var_uriref, RDF.type, FMI.Parameter))
+                        graph.add((fmu_uriref, FMI.hasParameter, var_uriref))
+                    else:
+                        continue
+                else:
+                    graph.add((var_uriref, RDF.type, FMI.Parameter))
+                    graph.add((fmu_uriref, FMI.hasParameter, var_uriref))
+            if var.causality == "input":
+                graph.add((var_uriref, RDF.type, FMI.Input))
+                graph.add((fmu_uriref, FMI.hasInput, var_uriref))
+            if var.causality == "output":
+                graph.add((var_uriref, RDF.type, FMI.Output))
+                graph.add((fmu_uriref, FMI.hasOutput, var_uriref))
+
+            graph.add((var_uriref, RDF.type, FMI.ScalarVariable))
+
+            if var.description != None:
+                graph.add(
+                    (var_uriref, DCT.description, rdflib.Literal(var.description))
+                )
+            if var.start != None:
+                graph.add((var_uriref, FMI.start, rdflib.Literal(var.start)))
+
+            graph = add_variable_constraints(graph, units_map, var_uriref, var)
+
+            if var.declaredType != None:
+                try:
+                    graph.add(
+                        (var_uriref, FMI.declaredType, types_map[var.declaredType.name])
+                    )
+                    graph = add_variable_constraints(
+                        graph, units_map, var_uriref, var.declaredType
+                    )
+                except KeyError:
+                    logger.warning(
+                        f"Type `{var.declaredType.name}` for variable `{var.name}` not found!"
+                    )
+                    continue
+
     # Serialize graph and write to disk
     output = "local/output.ttl"
     logger.info(f"Serializing graph to disk as `./{output}`...")
     graph.serialize(destination=output, format="ttl")
+
+
+def add_variable_constraints(graph, units_map, uriref, object):
+    try:
+        graph.add((uriref, FMI.unit, units_map[object.unit]))
+    except KeyError:
+        logger.warning(f"Unit `{object.unit}` for object `{object.name}` not found!")
+
+    if object.min != None:
+        graph.add((uriref, FMI.min, rdflib.Literal(object.min)))
+        # graph.add((uriref, QUDT.minInclusive, rdflib.Literal(object.min)))
+    if object.max != None:
+        graph.add((uriref, FMI.max, rdflib.Literal(object.max)))
+        # graph.add((uriref, QUDT.maxInclusive, rdflib.Literal(object.max)))
+    if object.nominal != None:
+        graph.add((uriref, FMI.nominal, rdflib.Literal(object.nominal)))
+
+    return graph
