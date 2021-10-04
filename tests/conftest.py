881c8d8a80484640c72a54bf46ae269ce4ff0c44
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-10-04 16:51:58 +0200

    Test expectations by reading queries and expected results from YAML file

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,21 @@
 
 """Provide global test fixtures for unit tests."""
 
+import os
 import sys
 
 import pytest
+import yaml
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import fmi2rdf  # noqa -- import has to happen _after_ modifying PATH
 
+
+def pytest_generate_tests(metafunc):
+
+    # Load questions from file
+    with open("tests/questions.yaml", "r") as fp:
+        collection = yaml.full_load(fp)
+
+    if "testcase" in metafunc.fixturenames:
+        metafunc.parametrize("testcase", collection["testcases"])
--- tests/test_competency.py
+++ tests/test_competency.py
@@ -4,3 +4,30 @@
 """Query ontology for answers to competency questions."""
 
 import os
+
+import invoke
+import pytest
+import rdflib
+
+from fmi2rdf import assemble_graph
+
+
+class TestCompetency(object):
+    def test_sparql_select(self, testcase):
+        fmu_path = os.path.abspath(f"./tests/data/{testcase['guid']}.fmu")
+        graph = assemble_graph(invoke.context.Context(), fmu_path, False)
+
+        a0 = graph.query(testcase["query"])
+
+        # Construct list of expected literal values
+        results = []
+        for row in a0:
+            for item in row:
+                results.append(item.n3().strip("<>"))
+
+        # Verify that each expected result is in the list of actual results
+        if results == []:
+            assert False
+        else:
+            for expected in testcase["expected"]:
+                assert expected in results
