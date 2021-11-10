009e733e11c478bac663640872520d076f95affb
Moritz Stüber <moritz.stueber@aut.uni-saarland.de>
2021-11-10 18:16:33 +0100

    Drop old approach of using RML for parsing---FMPy is better

--- fmi2rdf/fmi2rdf.py
+++ fmi2rdf/fmi2rdf.py
@@ -23,49 +23,6 @@ ABOX = rdflib.Namespace("https://abox.msaas.me/")
 
 @task(
     help={
-        "rules": "The full absolute path to the RML rules",
-        "serialization": "Serialization format (nquads/turtle/trig/trix/jsonld/hdt)",
-        "output": "The path to the output file",
-    },
-    optional=["serialization", "output"],
-)
-def apply_rml(ctx, rules, serialization="nquads", output=None):
-    """
-    Apply RML rules; generate output.
-
-    The target file on which the rules are applied is defined in the rules!
-    """
-    logger.debug(f"Applying RML mappings defined in '{os.path.basename(rules)}'...")
-
-    mapping = os.path.basename(rules)
-    workdir = os.path.dirname(rules)
-
-    prefix = (
-        "podman run -i --rm --name rmlmapper "
-        f"-v {workdir}:/data:Z "
-        "rmlio/rmlmapper-java:latest "
-    )
-
-    # TODO provenance info? https://github.com/RMLio/rmlmapper-java#generating-metadata
-
-    cmd_container = f"java rmlmapper.jar -m {mapping} -s {serialization}"
-    if output != None:
-        cmd_container += f" -o {output}"
-
-    cmd = prefix + cmd_container
-
-    logger.trace(cmd)
-    result = ctx.run(cmd, hide="out")
-
-    if result.ok:
-        logger.debug(f"\n{result.stdout}")
-        return result.stdout
-    else:
-        logger.warning(result)
-
-
-@task(
-    help={
         "fmu_path": "The full path to the FMU to be parsed",
         "blackbox": "Whether or not to include variables that are neither input nor ouptut",
     },
